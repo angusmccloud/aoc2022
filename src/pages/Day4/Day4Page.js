f86f785d7e6171f787439678bd316fc14f36ad9a
Connor Tyrrell <connor@brewgene.com>
2022-12-04 14:44:41 -0500

    Added day 4 solution

--- src/pages/Day4/Day4Page.js
+++ src/pages/Day4/Day4Page.js
@@ -11,13 +11,34 @@ const Day4Page = () => {
   const { taskNumber } = useParams();
 
   useEffect(() => {
-    setInputData(data.split('\n').map(v => parseInt(v.trim())));
+    setInputData(data.split('\n').map(v => v.trim()));
   }, []);
 
   useEffect(() => {
     // Solve Task 1
     if (taskNumber === '1' && result1 === '' && inputData.length > 0) {
-      // setResult1(`Number of Depth Increases: ${increasesCount}`);
+      let numberFullyCovered = 0;
+      inputData.forEach(v => {
+        // Split the row by the comma
+        const splitRow = v.split(',');
+        // Split the first row by the dash
+        const firstRow = splitRow[0].split('-');
+        // Split the second row by the dash
+        const secondRow = splitRow[1].split('-');
+
+        // Check if firstRow contained in secondRow
+        if (parseInt(firstRow[0]) >= parseInt(secondRow[0]) && parseInt(firstRow[1]) <= parseInt(secondRow[1])) {
+          numberFullyCovered++;
+          console.log("-- One contained in two --", splitRow);
+        } else if (parseInt(secondRow[0]) >= parseInt(firstRow[0]) && parseInt(secondRow[1]) <= parseInt(firstRow[1])) {
+          // Need this in an Else so we don't double-count things that are the same
+          numberFullyCovered++;
+          console.log("-- Two contained in one --", splitRow);
+        }
+      });
+      
+      
+      setResult1(`Number of Fully Covered Zones: ${numberFullyCovered}`);
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, [inputData]);
@@ -25,14 +46,31 @@ const Day4Page = () => {
   useEffect(() => {
     // Solve Task 2
     if (taskNumber === '2' && result2 === '' && inputData.length > 0) {
-      // setResult2(`Number of Summed Depth Increases: ${increasesCount}`);
+      let numberOverlap = 0;
+      inputData.forEach(v => {
+        // Split the row by the comma
+        const splitRow = v.split(',');
+        // Split the first row by the dash
+        const firstRow = splitRow[0].split('-');
+        // Split the second row by the dash
+        const secondRow = splitRow[1].split('-');
+
+        // Check if rows overlap
+        if ((parseInt(firstRow[0]) <= parseInt(secondRow[1]) && parseInt(firstRow[1]) >= parseInt(secondRow[0])) || (parseInt(firstRow[1]) <= parseInt(secondRow[1]) && parseInt(firstRow[0]) >= parseInt(secondRow[0]))) {
+          numberOverlap++;
+          console.log("-- One contained in two --", splitRow);
+        }
+      });
+      
+      
+      setResult1(`Number of Overlapping Zones: ${numberOverlap}`);
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, [inputData]);
 
   return (
     <div>
-      <Navigation pageName={`Day 9, Task ${taskNumber}`}>
+      <Navigation pageName={`Day 4, Task ${taskNumber}`}>
         {result1 !== '' && (
           <Typography variant='h5'>
             Result 1: {result1}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -62,7 +62,7 @@ const DatesTable = () => {
   const classes = useStyles();
 
   // Update this row as I do a new day
-  const completedDates = 3;
+  const completedDates = 4;
   let dateRows = [];
   for(let i = 1; i <= completedDates; i++) {
     dateRows.push(
