1f1c2e24427de93b7c623a83085e4a897a47895b
Connor Tyrrell <connor@brewgene.com>
2022-12-06 09:15:29 -0500

    Added day 6

--- src/pages/Day6/Day6Page.js
+++ src/pages/Day6/Day6Page.js
@@ -10,29 +10,51 @@ const Day6Page = () => {
   const [result2, setResult2] = useState('');
   const { taskNumber } = useParams();
 
+  const checkIfDuplicateExists = (arr) => {
+    return new Set(arr).size !== arr.length
+  }
+
   useEffect(() => {
-    setInputData(data.split('\n').map(v => parseInt(v.trim())));
+    setInputData(data.split('\n').map(v => v.trim()));
   }, []);
 
   useEffect(() => {
     // Solve Task 1
     if (taskNumber === '1' && result1 === '' && inputData.length > 0) {
-      // setResult1(`Number of Depth Increases: ${increasesCount}`);
+      const lettersList = inputData[0].split('');
+      const solveLength = 4;
+      for(let i = solveLength - 1; i < lettersList.length; i++) {
+        const checkArray = lettersList.slice(i-solveLength+1, i+1);
+        const hasDuplicates = checkIfDuplicateExists(checkArray);
+        if(!hasDuplicates) {
+          setResult1(`First unique Letter: ${lettersList[i]} ${i+1}`);
+          break;
+        }
+      }
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, [inputData]);
 
   useEffect(() => {
     // Solve Task 2
-    if (taskNumber === '2' && result2 === '' && inputData.length > 0) {
-      // setResult2(`Number of Summed Depth Increases: ${increasesCount}`);
+    if (taskNumber === '2' && result1 === '' && inputData.length > 0) {
+      const lettersList = inputData[0].split('');
+      const solveLength = 14;
+      for(let i = solveLength - 1; i < lettersList.length; i++) {
+        const checkArray = lettersList.slice(i-solveLength+1, i+1);
+        const hasDuplicates = checkIfDuplicateExists(checkArray);
+        if(!hasDuplicates) {
+          setResult1(`First unique Letter: ${lettersList[i]} ${i+1}`);
+          break;
+        }
+      }
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, [inputData]);
 
   return (
     <div>
-      <Navigation pageName={`Day 9, Task ${taskNumber}`}>
+      <Navigation pageName={`Day 6, Task ${taskNumber}`}>
         {result1 !== '' && (
           <Typography variant='h5'>
             Result 1: {result1}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -62,7 +62,7 @@ const DatesTable = () => {
   const classes = useStyles();
 
   // Update this row as I do a new day
-  const completedDates = 5;
+  const completedDates = 6;
   let dateRows = [];
   for(let i = 1; i <= completedDates; i++) {
     dateRows.push(
