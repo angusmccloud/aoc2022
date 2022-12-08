407eff31ce10204814da44f92508cadd64141f8d
Connor Tyrrell <connor@brewgene.com>
2022-12-08 06:45:33 -0500

    Added days 7 and 8

--- src/pages/Day7/Day7Page.js
+++ src/pages/Day7/Day7Page.js
@@ -6,33 +6,204 @@ import data from './input';
 
 const Day7Page = () => {
   const [inputData, setInputData] = useState([]);
+  const [parsedData, setParsedData] = useState([]);
   const [result1, setResult1] = useState('');
   const [result2, setResult2] = useState('');
   const { taskNumber } = useParams();
 
   useEffect(() => {
-    setInputData(data.split('\n').map(v => parseInt(v.trim())));
+    setInputData(data.split('\n').map(v => v.trim()));
   }, []);
 
   useEffect(() => {
+    if(inputData.length > 0) {
+      // Parse Data
+      const newParsedData = [];
+      let currentDirectory = 'baseDir';
+      let activeDirectory = {
+        name: 'baseDir',
+        children: [],
+        files: [],
+        totalFileSize: 0,
+        totalFileSizeWithChildren: 0,
+      };
+      let currentPath = [];
+
+      for(let i = 0; i < inputData.length; i++) {
+        const line = inputData[i];
+        const command = line.split(' ');
+        if(command[0] === '$') {
+          // We're listing or moving folders
+          if(command[1] === 'cd') {
+            // console.log('-- cd command --', command);
+            if(command[2] === '/') {
+              // We're moving to the root directory
+              currentPath = [];
+              currentPath.push('baseDir');
+            } else if (command[2] === '..') {
+              // We're moving up one directory
+              currentPath.pop();
+            } else {
+              // We're moving to a child directory
+              currentPath.push(command[2]);
+              currentDirectory = currentPath.join('/');
+              // currentDirectory = command[2];
+            }
+          } else if(command[1] === 'ls') {
+            // console.log('-- ls command, push activeDirectory --', activeDirectory);
+            if(currentDirectory !== activeDirectory.name) {
+              newParsedData.push(activeDirectory);
+            };
+            activeDirectory = {
+              name: currentDirectory,
+              children: [],
+              files: [],
+              totalFileSize: 0,
+              totalFileSizeWithChildren: 0,
+            };
+          }
+        } else if (command[0] === 'dir') {
+          // This is showing a folder that's a child in the current directory
+          const childFolder = `${currentPath.join('/')}/${command[1]}`;
+          activeDirectory.children.push(childFolder);
+        } else {
+          // This is showing a file that's a child in the current directory
+          const fileSize = parseInt(command[0]);
+          const fileName = command[1];
+          activeDirectory.files.push({
+            name: fileName,
+            size: fileSize,
+          });
+          activeDirectory.totalFileSize += fileSize;
+          activeDirectory.totalFileSizeWithChildren += fileSize;
+        }
+
+        // Don't miss the last row
+        if(i === inputData.length - 1) {
+          newParsedData.push(activeDirectory);
+        }
+      }
+      setParsedData(newParsedData);
+      console.log('-- newParsedData --', newParsedData);
+    }
+  }, [inputData])
+
+  useEffect(() => {
     // Solve Task 1
-    if (taskNumber === '1' && result1 === '' && inputData.length > 0) {
-      // setResult1(`Number of Depth Increases: ${increasesCount}`);
+    if (taskNumber === '1' && result1 === '' && parsedData.length > 0) {
+      let updatedParsedData = [...parsedData];
+      const folderWithChildrenSize = [];
+
+      let moreRecords = true;
+      while(moreRecords) {
+        const noChildren = updatedParsedData.filter((d) => d.children.length === 0);
+        if(noChildren.length === 0) {
+          moreRecords = false;
+          break;
+        }
+
+        for(let i = 0; i < noChildren.length; i++) {
+          const { name, totalFileSizeWithChildren: size } = noChildren[i];
+          folderWithChildrenSize.push({
+            name: name,
+            size: size,
+          });
+          // remove the folder so we don't see it again
+          updatedParsedData = updatedParsedData.filter((f) => f.name !== name);
+          // And then remove it as a child from its parent, AND add the size
+          updatedParsedData = updatedParsedData.map((f) => {
+            if(f.children.includes(name)) {
+              return {
+                name: f.name,
+                files: f.files,
+                totalFileSize: f.totalFileSize,
+                children: f.children.filter(c => c !== name),
+                totalFileSizeWithChildren: f.totalFileSizeWithChildren + size,
+              }
+            } else {
+              return {
+                ...f
+              }
+            }
+          });
+        };
+      }
+
+      // Sum size where size is <= 100000
+      let size = 0;
+      folderWithChildrenSize.forEach((folder) => {
+        if(folder.size <= 100000) {
+          size += folder.size;
+        }
+      });
+      setResult1(`Size of folders with children <= 100000: ${size}`);
+
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [inputData]);
+  }, [parsedData]);
 
   useEffect(() => {
     // Solve Task 2
-    if (taskNumber === '2' && result2 === '' && inputData.length > 0) {
-      // setResult2(`Number of Summed Depth Increases: ${increasesCount}`);
+    if (taskNumber === '2' && result2 === '' && parsedData.length > 0) {
+      let updatedParsedData = [...parsedData];
+      const folderWithChildrenSize = [];
+
+      let moreRecords = true;
+      while(moreRecords) {
+        const noChildren = updatedParsedData.filter((d) => d.children.length === 0);
+        if(noChildren.length === 0) {
+          moreRecords = false;
+          break;
+        }
+
+        for(let i = 0; i < noChildren.length; i++) {
+          const { name, totalFileSizeWithChildren: size } = noChildren[i];
+          folderWithChildrenSize.push({
+            name: name,
+            size: size,
+          });
+          // remove the folder so we don't see it again
+          updatedParsedData = updatedParsedData.filter((f) => f.name !== name);
+          // And then remove it as a child from its parent, AND add the size
+          updatedParsedData = updatedParsedData.map((f) => {
+            if(f.children.includes(name)) {
+              return {
+                name: f.name,
+                files: f.files,
+                totalFileSize: f.totalFileSize,
+                children: f.children.filter(c => c !== name),
+                totalFileSizeWithChildren: f.totalFileSizeWithChildren + size,
+              }
+            } else {
+              return {
+                ...f
+              }
+            }
+          });
+        };
+      };
+
+      let totalFileSize = 0;
+      for (let i = 0; i < parsedData.length; i++) {
+        totalFileSize += parsedData[i].totalFileSize;
+      }
+
+      const spaceAvailable = 70000000 - totalFileSize;
+      const targetDirectorySize = 30000000 - spaceAvailable;
+      
+      const bigEnoughFolders = folderWithChildrenSize.filter((f) => {
+        return f.size >= targetDirectorySize
+      });
+      const smallestBigFolder = bigEnoughFolders.sort((a, b) => a.size - b.size)[0].size;
+
+      setResult2(`Smallest Big Folder: ${smallestBigFolder}`);
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [inputData]);
+  }, [parsedData]);
 
   return (
     <div>
-      <Navigation pageName={`Day 9, Task ${taskNumber}`}>
+      <Navigation pageName={`Day 7, Task ${taskNumber}`}>
         {result1 !== '' && (
           <Typography variant='h5'>
             Result 1: {result1}
--- src/pages/Day8/Day8Page.js
+++ src/pages/Day8/Day8Page.js
@@ -6,33 +6,150 @@ import data from './input';
 
 const Day8Page = () => {
   const [inputData, setInputData] = useState([]);
+  const [dataGrid, setDataGrid] = useState([]);
   const [result1, setResult1] = useState('');
   const [result2, setResult2] = useState('');
   const { taskNumber } = useParams();
 
   useEffect(() => {
-    setInputData(data.split('\n').map(v => parseInt(v.trim())));
+    setInputData(data.split('\n').map(v => v.trim()));
   }, []);
 
   useEffect(() => {
-    // Solve Task 1
-    if (taskNumber === '1' && result1 === '' && inputData.length > 0) {
-      // setResult1(`Number of Depth Increases: ${increasesCount}`);
+    if(inputData.length > 0) {
+      const grid = [];
+      for (let i = 0; i < inputData.length; i++) {
+        const row = inputData[i].split('');
+        for(let ii = 0; ii < row.length; ii++) {
+          grid.push({
+            value: parseInt(row[ii]),
+            row: i,
+            column: ii,
+          });
+        }
+      }
+      setDataGrid(grid);
     }
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, [inputData]);
 
   useEffect(() => {
-    // Solve Task 2
-    if (taskNumber === '2' && result2 === '' && inputData.length > 0) {
-      // setResult2(`Number of Summed Depth Increases: ${increasesCount}`);
+    // Solve Task 1
+    if (taskNumber === '1' && result1 === '' && dataGrid.length > 0) {
+      // console.log('-- Data Grid --', dataGrid);
+      let visibleTreeCount = 0;
+      const minColumn = 0;
+      const minRow = 0;
+      const maxColumn = Math.max(...dataGrid.map(o => o.column));
+      const maxRow = Math.max(...dataGrid.map(o => o.row));
+
+      for (let i = 0; i < dataGrid.length; i++) {
+        const tree = dataGrid[i];
+        const { row, column, value: treeHeight } = tree;
+        if (row === minRow || row === maxRow || column === minColumn || column === maxColumn) {
+          // It's on an edge
+          visibleTreeCount++;
+        } else {
+          // Not on an edge, let's check the 4 directions
+          const left = dataGrid.filter(t => t.row === row && t.column < column);
+          const right = dataGrid.filter(t => t.row === row && t.column > column);
+          const up = dataGrid.filter(t => t.column === column && t.row < row);
+          const down = dataGrid.filter(t => t.column === column && t.row > row);
+          const maxLeft = Math.max(...left.map(o => o.value));
+          const maxRight = Math.max(...right.map(o => o.value));
+          const maxUp = Math.max(...up.map(o => o.value));
+          const maxDown = Math.max(...down.map(o => o.value));
+          if(treeHeight > maxLeft || treeHeight > maxRight || treeHeight > maxUp || treeHeight > maxDown) {
+            visibleTreeCount++;
+          }
+        }
+      };
+
+       setResult1(`Number of Visible Trees: ${visibleTreeCount}`);
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [inputData]);
+  }, [dataGrid]);
+
+  useEffect(() => {
+    // Solve Task 2
+    if (taskNumber === '2' && result2 === '' && dataGrid.length > 0) {
+      // console.log('-- Data Grid --', dataGrid);
+      let maxVisibleScore = 0;
+      const minColumn = 0;
+      const minRow = 0;
+      const maxColumn = Math.max(...dataGrid.map(o => o.column));
+      const maxRow = Math.max(...dataGrid.map(o => o.row));
+
+      for (let i = 0; i < dataGrid.length; i++) {
+        const tree = dataGrid[i];
+        const { row, column, value: treeHeight } = tree;
+        if (row === minRow || row === maxRow || column === minColumn || column === maxColumn) {
+          // It's on an edge, one of the multipliers is 0, skip it
+        } else {
+          // Not on an edge, let's check the 4 directions
+          const left = dataGrid.filter(t => t.row === row && t.column < column);
+          const right = dataGrid.filter(t => t.row === row && t.column > column);
+          const up = dataGrid.filter(t => t.column === column && t.row < row);
+          const down = dataGrid.filter(t => t.column === column && t.row > row);
+
+          let leftDistance = 0;
+          for(let i = left.length - 1; i >= 0; i--) {
+            const height = left[i].value;
+            if(height < treeHeight) {
+              leftDistance++
+            } else {
+              leftDistance++;
+              break;
+            }
+          };
+
+          let rightDistance = 0;
+          for(let i = 0; i < right.length; i++) {
+            const height = right[i].value;
+            if(height < treeHeight) {
+              rightDistance++
+            } else {
+              rightDistance++;
+              break;
+            }
+          };
+
+          let upDistance = 0;
+          for(let i = up.length - 1; i >= 0; i--) {
+            const height = up[i].value;
+            if(height < treeHeight) {
+              upDistance++
+            } else {
+              upDistance++;
+              break;
+            }
+          };
+
+          let downDistance = 0;
+          for(let i = 0; i < down.length; i++) {
+            const height = down[i].value;
+            if(height < treeHeight) {
+              downDistance++
+            } else {
+              downDistance++;
+              break;
+            }
+          };
+          const visibleScore = leftDistance * rightDistance * upDistance * downDistance;
+
+          // console.log('-- Distances --', leftDistance, rightDistance, upDistance, downDistance, visibleScore);
+          if(visibleScore > maxVisibleScore) { 
+            maxVisibleScore = visibleScore
+          }
+        }
+      };
+      setResult2(`Max Visible Score: ${maxVisibleScore}`);
+    }
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  }, [dataGrid]);
 
   return (
     <div>
-      <Navigation pageName={`Day 9, Task ${taskNumber}`}>
+      <Navigation pageName={`Day 8, Task ${taskNumber}`}>
         {result1 !== '' && (
           <Typography variant='h5'>
             Result 1: {result1}
