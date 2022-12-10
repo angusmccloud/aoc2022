ca522f2132ae59b8322a5b41a879c1335cb53847
Connor Tyrrell <connor@brewgene.com>
2022-12-10 08:15:08 -0500

    Day 9

--- src/pages/Day9/Day9Page.js
+++ src/pages/Day9/Day9Page.js
@@ -27,74 +27,89 @@ const Day9Page = () => {
       };
       setDirections(newDirections);
     }
-  }, [inputData])
+  }, [inputData]);
+  // 6023
+
+  const calcDistinctLocations = (directions, numberOfKnots) => {
+    const tailLocations = ['0|0'];
+    const locations = [];
+    for(let i = 0; i < numberOfKnots; i++) {
+      locations.push({x: 0, y: 0});
+    };
+
+    for(let i = 0; i < directions.length; i++) {
+      const direction = directions[i];
+      for(let ii = 0; ii < direction.numberSteps; ii++) {
+        // Move the Head first
+        if(direction.direction === 'R') {
+          locations[0].x += 1;
+        } else if(direction.direction === 'L') {
+          locations[0].x -= 1;
+        } else if(direction.direction === 'U') {
+          locations[0].y += 1;
+        } else if(direction.direction === 'D') {
+          locations[0].y -= 1;
+        }
+
+        // Then we loop through each knot in the tail
+        for(let ii = 1; ii < numberOfKnots; ii++) {
+          // Check if they're within 1 of each other
+          if(Math.abs(locations[ii].x - locations[ii-1].x) <= 1 && Math.abs(locations[ii].y - locations[ii-1].y) <= 1) {
+            // Tail doesn't move if within 1 of previous spot
+            continue;
+          }
+
+          if(locations[ii].y === locations[0].y) {
+            // We know same vertically, figure out if moving up or down
+            if(locations[ii].x < locations[ii-1].x) {
+              locations[ii].x += Math.abs(locations[ii].x - locations[ii-1].x) - 1;
+            } else {
+              locations[ii].x -= Math.abs(locations[ii].x - locations[ii-1].x) - 1;
+            }
+          } else if(locations[ii].x === locations[ii-1].x) {
+            // We know same horizontally, figure out if moving left or right
+            if(locations[ii].y < locations[ii-1].y) {
+              locations[ii].y += Math.abs(locations[ii].y - locations[ii-1].y) - 1;
+            } else {
+              locations[ii].y -= Math.abs(locations[ii].y - locations[ii-1].y) - 1;
+            }
+          } else {
+            // Otherwise we have to move the tail's X and Y
+            if(Math.abs(locations[ii].x - locations[ii-1].x) >= 2) {
+              locations[ii].y = locations[ii-1].y;
+              if(locations[ii].x < locations[ii-1].x) {
+                locations[ii].x += Math.abs(locations[ii].x - locations[ii-1].x) - 1;
+              } else {
+                locations[ii].x -= Math.abs(locations[ii].x - locations[ii-1].x) - 1;
+              }
+            } else if(Math.abs(locations[ii].y - locations[ii-1].y) >= 2) {
+              locations[ii].x = locations[ii-1].x;
+              if(locations[ii].y < locations[ii-1].y) {
+                locations[ii].y += Math.abs(locations[ii].y - locations[ii-1].y) - 1;
+              } else {
+                locations[ii].y -= Math.abs(locations[ii].y - locations[ii-1].y) - 1;
+              }
+            }
+          }
+
+          if(ii === numberOfKnots - 1) {
+            // Add the new location to the tail
+            tailLocations.push(`${locations[ii].x}|${locations[ii].y}`);
+          }
+        }
+      }
+      console.log('-- End of loop --', locations, tailLocations);
+    }
+
+    console.log('-- tailLocations --', new Set(tailLocations));
+    return new Set(tailLocations).size;
+  }
 
   useEffect(() => {
     // Solve Task 1
     if (taskNumber === '1' && result1 === '' && directions.length > 0) {
-      console.log('-- directions --', directions);
-      const tailLocations = ['0|0'];
-      let headLocation = {x: 0, y: 0};
-      let tailLocation = {x: 0, y: 0};
-      for(let i = 0; i < directions.length; i++) {
-        const direction = directions[i];
-        for(let ii = 0; ii < direction.numberSteps; ii++) {
-          if(direction.direction === 'R') {
-            headLocation.x += 1;
-          } else if(direction.direction === 'L') {
-            headLocation.x -= 1;
-          } else if(direction.direction === 'U') {
-            headLocation.y += 1;
-          } else if(direction.direction === 'D') {
-            headLocation.y -= 1;
-          }
-          // Check if they're within 1 of each other
-          if(Math.abs(tailLocation.x - headLocation.x) <= 1 && Math.abs(tailLocation.y - headLocation.y) <= 1) {
-            // Tail doesn't move if within 1 of head
-            // console.log('-- No Movement --', headLocation, tailLocation);
-            continue;
-          }
-
-          if(tailLocation.y === headLocation.y) {
-            // We know same vertically, figure out if moving up or down
-            if(tailLocation.x < headLocation.x) {
-              tailLocation.x += 1;
-            } else {
-              tailLocation.x -= 1;
-            }
-            // console.log('-- Moved X --', headLocation, tailLocation);
-            tailLocations.push(`${tailLocation.x}|${tailLocation.y}`);
-            continue;
-          }
-          if(tailLocation.x === headLocation.x) {
-            // We know same horizontally, figure out if moving up or down
-            if(tailLocation.y < headLocation.y) {
-              tailLocation.y += 1;
-            } else {
-              tailLocation.y -= 1;
-            }
-            // console.log('-- Moved Y --', headLocation, tailLocation);
-            tailLocations.push(`${tailLocation.x}|${tailLocation.y}`);
-            continue;
-          }
-
-          // Otherwise we have to move the tail's X and Y
-          if(tailLocation.x < headLocation.x) {
-            tailLocation.x += 1;
-          } else {
-            tailLocation.x -= 1;
-          }
-          if(tailLocation.y < headLocation.y) {
-            tailLocation.y += 1;
-          } else {
-            tailLocation.y -= 1;
-          }
-          // console.log('-- Moved Both --', headLocation, tailLocation);
-          tailLocations.push(`${tailLocation.x}|${tailLocation.y}`);
-        }
-      }
-      console.log("-- tailLocations --", tailLocations);
-      const numberOfSpots = new Set(tailLocations).size;
+      // console.log('-- directions --', directions);
+      const numberOfSpots = calcDistinctLocations(directions, 2);
       setResult1(`Number of Spots covered by tail: ${numberOfSpots}`);
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
@@ -103,7 +118,48 @@ const Day9Page = () => {
   useEffect(() => {
     // Solve Task 2
     if (taskNumber === '2' && result2 === '' && directions.length > 0) {
-      // setResult2(`Number of Summed Depth Increases: ${increasesCount}`);
+      // const numberOfSpots = calcDistinctLocations(directions, 10);
+      // setResult2(`Number of Spots covered by tail: ${numberOfSpots}`);
+      // Lower than 2575
+      // CORRECT: 2533
+
+    let body = new Array(10).fill(0).map(element => {
+        return { x: 0, y: 0 };
+    });
+
+    let direction = {
+        L: { x: -1, y:  0 },
+        R: { x:  1, y:  0 },
+        U: { x:  0, y: -1 },
+        D: { x:  0, y:  1 }
+    }
+
+    let positions = new Set(['0,0']);
+
+    directions.forEach(move => {
+        for (let step = 0; step < move.numberSteps; step++) {
+            body[0].x += direction[move.direction].x;
+            body[0].y += direction[move.direction].y;
+
+            for (let i = 1; i < body.length; i++) {
+                let distX = body[i - 1].x - body[i].x;
+                let distY = body[i - 1].y - body[i].y;
+
+                if (Math.abs(distX) >= 2) {
+                    body[i].x += Math.sign(distX);
+                    if (Math.abs(distY) != 0) body[i].y += Math.sign(distY);
+                } else if (Math.abs(distY) >= 2) {
+                    body[i].y += Math.sign(distY);
+                    if (Math.abs(distX) != 0) body[i].x += Math.sign(distX);
+                }
+            }
+
+            positions.add(`${body[body.length - 1].x},${body[body.length - 1].y}`);
+        }
+    })
+
+    // return positions.size;
+    setResult2(`Number of Spots covered by tail: ${positions.size}`);
     }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, [directions]);
